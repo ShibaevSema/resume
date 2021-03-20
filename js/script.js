aeefeec1bc0ed5dd1bed16dc034b9e290e10db3c
Sema Shibaev <seemon.shibaev@yandex.ru>
2021-03-20 18:52:44 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+ VANTA.BIRDS({
+        el: "#intro",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0xffffff,
+        color1: 0x36ea88,
+        color2: 0x1,
+        speedLimit: 2.00,
+        separation: 40.00,
+        quantity: 3.00
+    })
